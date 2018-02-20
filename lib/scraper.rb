0d5338735b34d8ee44052e1234a8295e99c0bba9
Kriti Rai <kriti.rai@gmail.com>
2018-02-20 06:03:46 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
   def get_courses
     self.get_page.css(".post")
   end
-  
+
   def make_courses
     self.get_courses.each do |post|
       course = Course.new
@@ -23,7 +23,7 @@ class Scraper
     end
   end
 
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
